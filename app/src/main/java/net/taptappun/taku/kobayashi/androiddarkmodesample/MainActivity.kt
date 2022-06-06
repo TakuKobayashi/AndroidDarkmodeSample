5e6d324e18ed5e5db3c9a4aa6a81991bec97428f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2022-06-06 21:05:36 +0900

    show switching imageview

--- app/src/main/java/net/taptappun/taku/kobayashi/androiddarkmodesample/MainActivity.kt
+++ app/src/main/java/net/taptappun/taku/kobayashi/androiddarkmodesample/MainActivity.kt
@@ -2,10 +2,14 @@ package net.taptappun.taku.kobayashi.androiddarkmodesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val iconImageView = findViewById<ImageView>(R.id.image_icon)
+        iconImageView.setImageResource(R.mipmap.setting_sample)
     }
 }
